a9f336b99b059de5e118b6668a08f0607a5510a4
liaodh <liaodh@jiguang.cn>
2019-01-23 17:12:06 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod vec2;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/vec2.rs
@@ -0,0 +1,74 @@
+#[allow(dead_code)]
+
+#[derive(Debug)]
+pub struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+impl Vec2 {
+    fn new(x: f64, y: f64) -> Vec2 {
+        return Vec2 { x, y };
+    }
+    fn data(&self) -> (f64, f64) {
+        return (self.x, self.y);
+    }
+    fn add(&mut self, rhs: Vec2) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+    fn sub(&mut self, rhs: Vec2) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+    fn mul(&mut self, rhs: Vec2) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+    }
+    fn dot(&self, rhs: Vec2) -> f64 {
+        return self.x * rhs.x + self.y * rhs.y;
+    }
+    fn length(&self) -> f64 {
+        self.lengthSq().sqrt()
+    }
+    fn lengthSq(&self) -> f64 {
+        return self.x * self.x + self.y * self.y;
+    }
+    fn normalize(&mut self) {
+        let sq = self.lengthSq();
+        let inv = 1.0 / sq;
+        self.x *= inv;
+        self.y *= inv;
+    }
+    fn scale(&mut self, scalar: f64) {
+        self.x *= scalar;
+        self.y *= scalar;
+    }
+
+    fn set(&mut self, x: f64, y: f64) {
+        self.x = x;
+        self.y = y;
+    }
+}
+
+#[test]
+fn vec2_add() {
+    let mut a = Vec2::new(0.0, 0.0);
+    let b = Vec2::new(1.0, 1.0);
+    a.add(b);
+    assert_eq!(a.x, 1.0);
+    assert_eq!(a.y, 1.0);
+}
+
+#[test]
+fn vec2_print() {
+    let a = Vec2::new(3.0, 4.0);
+    println!("{:?}", a);
+    assert_eq!(a.length(), 5.0);
+}
+
+#[test]
+fn vec2_length() {
+    let a = Vec2::new(3.0, 4.0);
+    assert_eq!(a.length(), 5.0);
+}
