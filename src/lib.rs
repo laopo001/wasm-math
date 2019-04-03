81c06da86f9f1933deea87e1227c8718b7b94c25
laopo001 <353272497@qq.com>
2019-04-03 22:29:13 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(box_into_raw_non_null)]
+#![feature(box_syntax)]
 extern crate cfg_if;
 
 use cfg_if::cfg_if;
@@ -9,12 +11,12 @@ use wasm_bindgen::prelude::*;
 pub mod mat3;
 pub mod mat4;
 pub mod math;
+pub mod node;
 pub mod quat;
+pub mod test;
 pub mod vec2;
 pub mod vec3;
 pub mod vec4;
-pub mod node;
-pub mod test;
 // pub use self::vec2::Vec2;
 
 #[wasm_bindgen]
@@ -29,5 +31,12 @@ cfg_if! {
         pub fn main() {
             log("wasm-math loaded");
         }
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
 }
